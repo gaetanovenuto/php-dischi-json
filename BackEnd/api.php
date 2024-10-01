5431ebc91d55df85a89c366e7bebd8a569021b92
gaetanovenuto <gaetanovenuto99@live.it>
2024-10-01 13:59:56 +0200

    Esercizio completato

--- BackEnd/api.php
+++ BackEnd/api.php
@@ -3,7 +3,17 @@
 /* PER IL FUTURO */
 // header("Access-Control-Allow-Origin: http://127.0.0.1:5173");
 // header("Access-Control-Allow-Headers: X-Requested-With");
+
+$disksString = file_get_contents('db/data.json');
+$disksArray = json_decode($disksString, true);
+
+$disks = [];
+
+foreach ($disksArray as $index => $singleDisk) {
+    $disks[] = $singleDisk;
+}
+
 	
 header('Content-Type: application/json');
  
-echo json_encode($students);
+echo json_encode($disks);
--- FrontEnd/js/scripts.js
+++ FrontEnd/js/scripts.js
@@ -3,13 +3,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-           
+           disks: []
         };
     },
     created() {
-        
+        this.search();
     },
     methods: {
-        
+        search() {
+            axios
+                .get('http://localhost/php-dischi-json/backend/api.php')
+                .then((res) => {
+                    console.log(res.data);
+
+                    this.disks = res.data;
+                });
+        }
     }
 }).mount('#app');
